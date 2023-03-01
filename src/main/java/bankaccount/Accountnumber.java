fef12d304dce2426d0b424faed86e0d7479b9e9c
tringapps-aravindhankl <123533867+tringapps-aravindhankl@users.noreply.github.com>
2023-03-01 17:22:09 +0530

    Update Accountnumber.java

--- src/main/java/bankaccount/Accountnumber.java
+++ src/main/java/bankaccount/Accountnumber.java
@@ -50,7 +50,7 @@ class Accountdetails {
     {
     	 int choose=0;
     	 do{
-             System.out.println("1.deposite 2.withdraw 3.balance 4.Exit");
+             log.info("1.deposite 2.withdraw 3.balance 4.Exit");
              choose = scan.nextInt();
              
              switch(choose) {
